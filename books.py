bb878140bdd6cdd03aaf778f26cbc66e125c2e95
April Cheng <april.sh.cheng@gmail.com>
2021-04-09 16:35:35 -0700

    Add choice_checker, ask_for_query, search_book functions

--- books.py
+++ books.py
@@ -8,6 +8,10 @@ def MAIN_MENU_OPTIONS():
     return ["Search for a book", "Move a book", "Quit"]
 
 
+def SEARCH_MENU_OPTIONS():
+    return ["Author", "Title", "Publisher", "Shelf", "Category", "Subject"]
+
+
 def dump_dictionary_to_file(converted_json):
     with open('somebooks.json', 'w+') as file_object:
         dump(converted_json, file_object)
@@ -21,8 +25,10 @@ def convert_excel_to_json(excel_file):
 
 
 def choice_checker(option_list):
-    print(list(enumerate(option_list, start=1)))
-    user_choice = int(input("What would you like to do? Enter a number: "))
+    choice_list = list(enumerate(option_list, start=1))
+    for choice in choice_list:
+        print(f'{choice[0]}. {choice[1]}')
+    user_choice = int(input("Enter a number: "))
     number_list = [number + 1 for number in range(len(option_list))]
     while user_choice not in number_list:
         print(f'Input is not an option, please try again.')
@@ -30,14 +36,29 @@ def choice_checker(option_list):
     return user_choice
 
 
+def ask_for_query(user_choice, menu_option):
+    user_query = input(f'Enter the {menu_option[user_choice - 1]} to search: ')
+    return user_query
+
+
+def search_book():
+    print(f'What would like to search for?')
+    user_choice = choice_checker(SEARCH_MENU_OPTIONS())
+    user_query = ask_for_query(user_choice, SEARCH_MENU_OPTIONS())
+
+search_book()
+
+
 def main_menu_selection():
+    print(f'What would you like to do?')
     user_choice = choice_checker(MAIN_MENU_OPTIONS())
     if user_choice == 1:
-        return 1
+        search_book()
     elif user_choice == 2:
         return 2
-    else:
-        return "quit"
+    return "quit"
+
+# main_menu_selection()
 
 
 def book_manager():
@@ -47,13 +68,9 @@ def book_manager():
             pass
     else:
         convert_excel_to_json("somebooks.xlsx")
-    while True:
-        main_menu()
-
-
-
-# def book_manager():
-#     check_json_file()
+    user_choice = main_menu_selection()
+    while user_choice != "quit":
+        break
 
 
 def main():
