66a1f3e6bf34b6fff14e107a6aaca4b1b05c79ec
April Cheng <april.sh.cheng@gmail.com>
2021-04-09 13:50:06 -0700

    Add __init__ for book.py

---
+++ book.py
@@ -0,0 +1,10 @@
+class Book:
+
+    def __init__(self, author: str, title: str, publisher: str, shelf: int, category: str, subject: str):
+        self.author = author
+        self.title = title
+        self.publisher = publisher
+        self.shelf = shelf
+        self.category = category
+        self.subject = subject
+
