7ea5ceadc5a5dc2989f0da79eaa3f6a4ecd5c759
April Cheng <april.sh.cheng@gmail.com>
2021-04-09 22:09:47 -0700

    Add color to __str__

--- book.py
+++ book.py
@@ -9,5 +9,7 @@ class Book:
         self.subject = subject
 
     def __str__(self) -> str:
-        return f'Author: {self.author}, Title: {self.title}, Publisher: {self.publisher}, Shelf: {self.shelf}, ' \
-               f'Category: {self.category}, Subject: {self.subject} '
+        return f'\u001b[32;1mAuthor\u001b[0m: {self.author}, \u001b[32;1mTitle\u001b[0m: {self.title}, \u001b[' \
+               f'32;1mPublisher\u001b[0m: {self.publisher}, \u001b[32;1mShelf\u001b[0m: {self.shelf}, ' \
+               f'\u001b[32;1mCategory\u001b[0m: {self.category}, \u001b[32;1mSubject\u001b[0m: {self.subject} '
+
