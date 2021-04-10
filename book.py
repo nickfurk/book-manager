ef297541e3598c76cdb4afef615f33b19b58e592
April Cheng <april.sh.cheng@gmail.com>
2021-04-10 00:16:14 -0700

    Add id attribute to Book

--- book.py
+++ book.py
@@ -1,6 +1,7 @@
 class Book:
 
-    def __init__(self, author: str, title: str, publisher: str, shelf: int, category: str, subject: str):
+    def __init__(self, id: str, author: str, title: str, publisher: str, shelf: str, category: str, subject: str):
+        self.id = id
         self.author = author
         self.title = title
         self.publisher = publisher
@@ -11,5 +12,6 @@ class Book:
     def __str__(self) -> str:
         return f'\u001b[32;1mAuthor\u001b[0m: {self.author}, \u001b[32;1mTitle\u001b[0m: {self.title}, \u001b[' \
                f'32;1mPublisher\u001b[0m: {self.publisher}, \u001b[32;1mShelf\u001b[0m: {self.shelf}, ' \
-               f'\u001b[32;1mCategory\u001b[0m: {self.category}, \u001b[32;1mSubject\u001b[0m: {self.subject} '
+               f'\u001b[32;1mCategory\u001b[0m: {self.category}, \u001b[32;1mSubject\u001b[0m: {self.subject}, ' \
+               f'\u001b[32;1mid\u001b[0m: {self.id} '
 
