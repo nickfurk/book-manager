2ce0db3ad0611399f9afa70703cdca281fd4a3ea
April Cheng <april.sh.cheng@gmail.com>
2021-04-13 12:35:38 -0700

    Add test_move_shelf_update_shelf_print_string and test_move_main_menu_selection_called

--- test_move_book.py
+++ test_move_book.py
@@ -51,7 +51,7 @@ class TestMoveBook(TestCase):
         mock_input_error_retry.assert_called_with(user_input, verify_filtered_list)
 
     @patch('books.main_menu_selection')
-    @patch('books.shelf_input_retry_and_convert', return_value=None)
+    @patch('books.shelf_input_retry_and_convert', return_value=9)
     @patch('books.input_error_retry')
     @patch('builtins.input', side_effect=["1", "1"])
     @patch('books.move_book_no_result_retry',
@@ -65,27 +65,77 @@ class TestMoveBook(TestCase):
         book_collection_list = [book_1]
         move_book(book_collection_list)
         selected_book = book_1
-        str = mock_output.getvalue()
-        str_split = str.split('\n')[0:3]
+        output_str = mock_output.getvalue()
+        str_split = output_str.split('\n')[0:3]
         actual = "".join(str_split)
-        expected = f'You have selected >>> {selected_book}Which shelf do you want to move the book to? See options ' \
-                   f'below: '
+        expected = f'You have selected >>> {selected_book}Which shelf do you want to move the book to? See ' \
+                   f'options below:'
         self.assertEqual(actual, expected)
 
-    def test_move_shelf_options_called(self):
-        pass
+    @patch('books.main_menu_selection')
+    @patch('books.shelf_input_retry_and_convert')
+    @patch('books.shelf_choices')
+    @patch('books.input_error_retry')
+    @patch('builtins.input', side_effect=["1", "10"])
+    @patch('books.move_book_no_result_retry')
+    @patch('books.search_book')
+    def test_move_shelf_choices_called(self, mock_search_book, mock_move_book_no_result_retry, mock_input,
+                                       mock_input_error_retry, mock_shelf_choices, mock_shelf_input_retry_and_convert,
+                                       mock_main_menu_selection):
+        book_1 = Book("123", "April C", "Hacking with April", "Chris", "9", "science", "computer")
+        book_collection_list = [book_1]
+        move_book(book_collection_list)
+        mock_shelf_choices.assert_called_with(book_collection_list)
 
-    def test_move_user_shelf_input_print_string(self):
-        pass
+    @patch('books.main_menu_selection')
+    @patch('books.shelf_input_retry_and_convert')
+    @patch('books.shelf_choices')
+    @patch('books.input_error_retry')
+    @patch('builtins.input')
+    @patch('books.move_book_no_result_retry')
+    @patch('books.search_book')
+    def test_shelf_input_retry_and_convert_called(self, mock_search_book, mock_move_book_no_result_retry, mock_input,
+                                                  mock_input_error_retry, mock_shelf_choices,
+                                                  mock_shelf_input_retry_and_convert,
+                                                  mock_main_menu_selection):
+        book_1 = Book("123", "April C", "Hacking with April", "Chris", "9", "science", "computer")
+        book_collection_list = [book_1]
+        move_book(book_collection_list)
+        user_shelf_input = (mock_input()).title()
+        shelf_options = str(mock_shelf_choices())
+        mock_shelf_input_retry_and_convert.assert_called_with(user_shelf_input, shelf_options)
 
-    def test_move_verify_user_shelf_input_called(self):
-        pass
+    @patch('books.main_menu_selection')
+    @patch('books.shelf_input_retry_and_convert', return_value=2)
+    @patch('books.shelf_choices')
+    @patch('books.input_error_retry')
+    @patch('builtins.input', side_effect=["1", "1"])
+    @patch('books.move_book_no_result_retry',
+           return_value=[Book("123", "April C", "Hacking with April", "Chris", "9", "science", "computer")])
+    @patch('books.search_book')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_move_shelf_update_shelf_print_string(self, mock_output, mock_search_book, mock_move_book_no_result_retry,
+                                                 mock_input, mock_input_error_retry, mock_shelf_choices,
+                                                 mock_shelf_input_retry_and_convert, mock_main_menu_selection):
+        book_1 = Book("123", "April C", "Hacking with April", "Chris", "9", "science", "computer")
+        book_collection_list = [book_1]
+        move_book(book_collection_list)
+        selected_book = book_1
+        selected_book.shelf = mock_shelf_input_retry_and_convert(2, [1, 2, 3])
+        output_str = mock_output.getvalue()
+        str_split = output_str.split('\n')[5]
+        actual = "".join(str_split)
+        expected = f'\u001b[33;1mBook shelf updated!\u001b[0m >>> {selected_book}'
+        self.assertEqual(actual, expected)
 
-    def test_move_selected_book_updates(self):
-        pass
-
-    def test_move_shelf_update_print_string(self):
-        pass
-
-    def test_move_main_menu_selection_called(self):
-        pass
+    @patch('books.main_menu_selection')
+    @patch('books.shelf_input_retry_and_convert')
+    @patch('books.input_error_retry')
+    @patch('builtins.input', side_effect=[None, '1'])
+    @patch('books.search_book')
+    def test_move_main_menu_selection_called(self, mock_search_book, mock_input, mock_input_error_retry,
+                                          mock_shelf_input_retry_and_convert, mock_main_menu_selection):
+        book_1 = Book("123", "April C", "Hacking with April", "Chris", "9", "science", "computer")
+        book_collection_list = [book_1]
+        move_book(book_collection_list)
+        mock_main_menu_selection.assert_called_with(book_collection_list)
