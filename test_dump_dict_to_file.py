18394b7ed6542f5447a1b883d338cf76189513b4
April Cheng <april.sh.cheng@gmail.com>
2021-04-13 07:38:08 -0700

    Add test_dump_dict_to_file_print_goodbye_message to test_dump_dict_to_file.py

--- test_dump_dict_to_file.py
+++ test_dump_dict_to_file.py
@@ -1,3 +1,4 @@
+import io
 from unittest import TestCase
 from unittest.mock import patch, mock_open
 from books import dump_dict_to_file, JSON_FILENAME
@@ -17,5 +18,8 @@ class TestDumpDictToFile(TestCase):
             mock_file = mocked_open()
             mock_dump.assert_called_once_with({}, mock_file,  indent=4)
 
-
-
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_dump_dict_to_file_print_goodbye_message(self, mock_output):
+        dump_dict_to_file({})
+        expected = f'\n\u001b[33;1mData saved. See you later!\u001b[0m\n\n'
+        self.assertEqual(mock_output.getvalue(), expected)
