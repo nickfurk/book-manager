e51aa27607276ef666954d30b43209a7d314b2e1
April Cheng <april.sh.cheng@gmail.com>
2021-04-14 18:44:45 -0700

    Edit unittests

--- test_get_book_collection.py
+++ test_get_book_collection.py
@@ -5,22 +5,22 @@ from books import get_book_collection
 
 class TestGetBookCollection(TestCase):
 
-    @patch('books.create_book_object')
+    @patch('books.read_from_json_file')
     @patch('books.pathlib.Path', return_value=Mock())
-    def test_create_book_object_is_called(self, mock_pathlib_path, mock_create_book_object):
+    def test_get_book_collection_read_book_object_called(self, mock_pathlib_path, mock_read_from_json_file):
         path = mock_pathlib_path.return_value
         path.exists.return_value = True
         get_book_collection()
-        mock_create_book_object.assert_called_with()
+        mock_read_from_json_file.assert_called_with()
 
     @patch('books.convert_excel_to_json')
     @patch('books.pathlib.Path', return_value=Mock())
-    def test_convert_excel_to_json_is_called(self, mock_pathlib_path, mock_convert_excel_to_json):
+    def test_get_book_collection_convert_excel_to_json_is_called(self, mock_pathlib_path, mock_convert_excel_to_json):
         path = mock_pathlib_path.return_value
         path.exists.return_value = False
         get_book_collection()
         mock_convert_excel_to_json.assert_called_with('somebooks.xlsx')
 
-    def test_return_a_list(self):
+    def test_get_book_collection_return_a_list(self):
         actual = get_book_collection()
         self.assertEqual(type(actual), list)
